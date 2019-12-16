98beac513b67a613e23186f9e7d06a5344d495d0
msharrock <mfsharrock@gmail.com>
2019-12-16 05:43:29 -0500

    fix 2

--- preprocess/extract.py
+++ preprocess/extract.py
@@ -23,14 +23,14 @@ Params:
 Output: 
         - brain_image: nifti object, extracted brain
 '''
-    tmpfile = 'tmpfile.nii.gz'
-    image.to_filename(tmpfile)
-    mask = fslmaths(image).thr('0.000000').uthr('100.000000').bin().fillh().run()
-    fslmaths(image).mas(mask).run(tmpfile)
-    bet(tmpfile, tmpfile, fracintensity = 0.01)
-    mask = fslmaths(tmpfile).bin().fillh().run()
-    brain_image = fslmaths(image).mas(mask).run()
-    os.remove(tmpfile)
+        tmpfile = 'tmpfile.nii.gz'
+        image.to_filename(tmpfile)
+        mask = fslmaths(image).thr('0.000000').uthr('100.000000').bin().fillh().run()
+        fslmaths(image).mas(mask).run(tmpfile)
+        bet(tmpfile, tmpfile, fracintensity = 0.01)
+        mask = fslmaths(tmpfile).bin().fillh().run()
+        brain_image = fslmaths(image).mas(mask).run()
+        os.remove(tmpfile)
 
-    return brain_image
+        return brain_image
 
