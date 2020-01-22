39cdd9bcc631504f36893583ad76c3f2b4a4ea5d
msharrock <mfsharrock@gmail.com>
2020-01-22 11:57:26 -0500

    updated blocks

--- blocks/vnet.py
+++ blocks/vnet.py
@@ -28,43 +28,70 @@ class VNetInBlock(layers.Layer):
 
 
 class VNetDownBlock(layers.Layer):
-    def __init__(self, channels, n_convs):
+    def __init__(self, channels, n_convs, norm=False, drop=False, training=False):
         super(VNetDownBlock, self).__init__()
         self.channels = channels
         self.n_convs = n_convs
+        self.training = training
+        self.norm = norm
+        self.drop = drop
         self.add = layers.Add()
         self.downsample = layers.Conv3D(filters=self.channels, kernel_size=(2,2,2), strides=2,
-                                         padding='valid', kernel_initializer='he_normal', activation='relu')
+                                         padding='valid', kernel_initializer='he_normal', activation=None)
         self.convolution = layers.Conv3D(filters=self.channels, kernel_size=(5,5,5), strides=1, 
-                                         padding='same', kernel_initializer='he_normal', activation='relu') 
-
+                                         padding='same', kernel_initializer='he_normal', activation=None) 
+        self.batch_norm = layers.BatchNormalization(scale=False, renorm=True, trainable=self.training)
+        self.activation = layers.Activation('relu')
+        self.dropout = layers.Dropout(0.1)
+        
     def call(self, inputs):  
         d = self.downsample(inputs) 
+        if self.norm:
+            d = self.batch_norm(d, training=self.training)
+        d = self.activation(d)
+        x = d
         
         for _ in range(self.n_convs):
-            x = self.convolution(d)
+            x = self.convolution(x)
+            x = self.activation(x)
+            if self.dropout:
+                x = self.dropout(x, training=self.training)
             
         return self.add([x, d])  
 
 class VNetUpBlock(layers.Layer):
-    def __init__(self, channels, n_convs):
+    def __init__(self, channels, n_convs, norm=False, drop=False, training=False):
         super(VNetUpBlock, self).__init__()
         self.channels = channels
         self.n_convs = n_convs
+        self.training = training
+        self.norm = norm
+        self.drop = drop
         self.add = layers.Add() 
         self.concatenate = layers.Concatenate() 
         self.upsample = layers.Conv3DTranspose(filters=self.channels//2, kernel_size=(2,2,2), strides=2,
-                                               padding='valid', kernel_initializer='he_normal', activation='relu')
+                                               padding='valid', kernel_initializer='he_normal', activation=None)
         self.convolution = layers.Conv3D(filters=self.channels, kernel_size=(5,5,5), strides=1, 
-                                         padding='same', kernel_initializer='he_normal', activation='relu') 
-
+                                         padding='same', kernel_initializer='he_normal', activation=None) 
+        self.batch_norm = layers.BatchNormalization(scale=False, renorm=True, trainable=self.training)
+        self.activation = layers.Activation('relu')
+        self.dropout = layers.Dropout(0.1)
+        
+        
     def call(self, inputs, skip):  
         x = self.upsample(inputs)
+        if self.norm:
+            x = self.batch_norm(x, training=self.training)
+        x = self.activation(x)
         cat = self.concatenate([x, skip])
+        x = cat
         
         for _ in range(self.n_convs):
-            x = self.convolution(cat)
-                   
+            x = self.convolution(x)
+            x = self.activation(x)
+            if self.drop:
+                x = self.dropout(x, training=self.training)
+            
         return self.add([x, cat])  
 
 
@@ -82,4 +109,3 @@ class VNetOutBlock(layers.Layer):
         x = self.final(inputs)
 
         return self.binary(x)
-
