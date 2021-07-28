9745d9bac0701ba398a5762e939d5ccda65882a9
Christopher Lin <clin5@bidmc.harvard.edu>
2021-07-28 14:47:04 -0400

    Copy original headers to prediciton output

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@
 
 
 """
-Prediction Script for DeepBleed 
+Prediction Script for DeepBleed
 
 Command Line Arguments:
         --indir: string, location to perform prediction
@@ -17,7 +17,7 @@ Command Line Arguments:
 
 import os
 import fsl
-import ants 
+import ants
 import time
 import nibabel as nib
 import tensorflow as tf
@@ -43,12 +43,12 @@ if setup.GPUS:
 else:
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-# set paths to scripts, templates, weights etc. 
+# set paths to scripts, templates, weights etc.
 TEMPLATE_PATH = os.path.join('templates', 'scct_unsmooth_SS_0.01_128x128x128.nii.gz')
 
 if setup.weights:
     WEIGHT_PATH = setup.weights
-else: 
+else:
     WEIGHT_PATH = 'weights'
 
 # load the model and weights
@@ -57,12 +57,12 @@ model.load_weights(WEIGHT_PATH)
 
 # setup directory trees
 IN_DIR = setup.IN_DIR
-OUT_DIR = setup.OUT_DIR 
+OUT_DIR = setup.OUT_DIR
 if not os.path.exists(IN_DIR):
     os.mkdir(IN_DIR)
 if not os.path.exists(OUT_DIR):
     os.mkdir(OUT_DIR)
-    
+
 # load input data
 files = sorted(next(os.walk(IN_DIR))[2])
 files = [os.path.join(IN_DIR, f) for f in files]
@@ -70,31 +70,33 @@ template = ants.image_read(TEMPLATE_PATH, pixeltype = 'float')
 
 for filename in files:
 
-    # preprocessing   
+    # preprocessing
     if verbose:
         timestamp = time.time()
-        print('loading:', filename)  
+        print('loading:', filename)
     original_image = nib.load(filename)
+    original_header = original_image.header
+    original_affine = original_image.affine
     original_image = convert.nii2ants(original_image)
-    
-    
+
+
     if brain_only:
         image = original_image
-    else:        
+    else:
         image = nib.load(filename)
         if verbose:
-            print('brain extraction') 
+            print('brain extraction')
         image = extract.brain(image)
         image = convert.nii2ants(image)
 
     if verbose:
-        print('template registration')  
+        print('template registration')
     image, transforms = register.rigid(template, image)
     image, ants_params = convert.ants2np(image)
 
-    # neural net prediction 
+    # neural net prediction
     if verbose:
-        print('generating prediction')   
+        print('generating prediction')
     prediction = model.predict(image)
 
     # invert registration
@@ -103,7 +105,7 @@ for filename in files:
     if verbose:
         print('inverting registration')
     prediction = register.invert(original_image, prediction, transforms)
-    prediction = convert.ants2nii(prediction)
+    prediction = nib.Nifti1Image(prediction.numpy(), header=original_header, affine=original_affine)
 
     if verbose:
         print('saving:', os.path.join(OUT_DIR, os.path.basename(filename)))
